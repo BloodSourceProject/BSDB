126e21ac424d7c145cbe59bc6e6c8d784b261d28
Attila <ati.nagy666@gmail.com>
2016-04-23 22:47:32 +0200

    Fix possible errors at Merge

--- Dungeons/Skyreach/access_requirement_2016_04_23_01.sql
+++ Dungeons/Skyreach/access_requirement_2016_04_23_01.sql
@@ -1,3 +1,3 @@
 -- Norm és HC megnyitása
-UPDATE `access_requirement` SET `level_min`='96' WHERE (`mapId`='1209') AND (`difficulty`='1')
-UPDATE `access_requirement` SET `level_min`='100' WHERE (`mapId`='1209') AND (`difficulty`='2')
+UPDATE `access_requirement` SET `level_min`='96' WHERE (`mapId`='1209') AND (`difficulty`='1');
+UPDATE `access_requirement` SET `level_min`='100' WHERE (`mapId`='1209') AND (`difficulty`='2');
