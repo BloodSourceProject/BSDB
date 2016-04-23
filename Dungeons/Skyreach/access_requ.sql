afb362da1fa8c8e457e013a4168d47100ef196e9
Attila <ati.nagy666@gmail.com>
2016-04-23 16:51:29 +0200

    Typo

--- Dungeons/Skyreach/access_requ.sql
+++
@@ -1,3 +0,0 @@
--- Norm és HC megnyitása
-UPDATE `access_requirement` SET `level_min`='96' WHERE (`mapId`='1209') AND (`difficulty`='1')
-UPDATE `access_requirement` SET `level_min`='100' WHERE (`mapId`='1209') AND (`difficulty`='2')
