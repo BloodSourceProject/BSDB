97800bba1d4ab8ca07976b532a761e62f8d8a383
Attila <ati.nagy666@gmail.com>
2016-04-25 11:12:42 +0200

    Skyreach: Solar Familiar (SAI)

---
+++ Dungeons/Skyreach/Trash/mart_ai_solar_familiar.sql
@@ -0,0 +1,10 @@
+-- Solar Familiar
+SET @ENTRY := 76097;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,0,7000,7000,20000,20000,11,152953,4,0,0,0,0,5,0,0,0,0.0,0.0,0.0,0.0,"Flash Bang - 7s"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,13000,13000,20000,20000,11,159290,4,0,0,0,0,5,0,0,0,0.0,0.0,0.0,0.0,"Empower - 13s"),
+(@ENTRY,@SOURCETYPE,2,0,59,0,100,0,0,0,0,0,11,160066,4,0,0,0,0,5,0,0,0,0.0,0.0,0.0,0.0,"Flash Bang - Effect - 9,1s");
